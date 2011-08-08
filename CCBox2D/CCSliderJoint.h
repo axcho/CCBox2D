259e5142d21a7b1bb544abd7b93ef314d413ce7e
Chris Lowe <chris.scott.lowe@gmail.com>
2011-08-07 22:58:15 -0500

    Initial Commit
    Replaced CCSlideJoint with CCSliderJoint

---
+++ CCBox2D/CCSliderJoint.h
@@ -0,0 +1,16 @@
+//
+//  CCSliderJoint.h
+//  CCBox2DJoints
+//
+//  Created by Chris Lowe on 8/7/11.
+//  Copyright 2011 Chris Lowe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CCWorldLayer.h"
+
+@interface CCSliderJoint : CCSprite {
+    
+}
+
+@end
--- CCBox2D/CCSliderJoint.mm
+++ CCBox2D/CCSliderJoint.mm
@@ -1,14 +1,14 @@
 //
-//  CCSlideJoint.m
+//  CCSliderJoint.m
 //  CCBox2DJoints
 //
 //  Created by Chris Lowe on 8/7/11.
 //  Copyright 2011 Chris Lowe. All rights reserved.
 //
 
-#import "CCSlideJoint.h"
+#import "CCSliderJoint.h"
 
 
-@implementation CCSlideJoint
+@implementation CCSliderJoint
 
 @end
