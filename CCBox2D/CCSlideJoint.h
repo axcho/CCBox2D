2cdaf4cebeab627e926b13c687d5d0539f1a118c
Chris Lowe <chris.scott.lowe@gmail.com>
2011-08-07 21:25:20 -0500

    Adding support for Prismatic Joints
    CCSlideJoint is an Objective-C wrapper (as part of CCBox2D) that adds
    support for Prismatic Joints in Box2D.  A Prismatic Joint is a joint
    that is fixed along one axis (for example, an object an "rotate" along a
    Y axis only while having a fixed X, or vice versa).  This is especially
    useful in vehicle objects where you have 2 turning wheels and 2 fixed
    wheels - The prismatic joint would be the fixed wheels.

---
+++ CCBox2D/CCSlideJoint.h
@@ -0,0 +1,34 @@
+//
+//  CCSlideJoint.h
+//  CCBox2DJoints
+//
+//  Created by Chris Lowe on 8/7/11.
+//  Copyright 2011 Chris Lowe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CCWorldLayer.h"
+
+@interface CCSlideJoint : CCSprite {
+    
+	BOOL _fixed;
+	BOOL _running, _limited;
+	float _motorSpeed, _maxTorque, _minRotation, _maxRotation;
+	CGPoint _anchor;
+	b2PrismaticJoint *_prismaticJoint;
+	CCBodySprite *_body1;
+	CCBodySprite *_body2;
+	CCWorldLayer *_world;
+}
+
+@property (nonatomic) BOOL running;
+@property (nonatomic) BOOL limited;
+@property (nonatomic) float speed;
+@property (nonatomic) float power;
+@property (nonatomic) float minRotation;
+@property (nonatomic) float maxRotation;
+
+-(void) setBody:(CCBodySprite *)sprite1 andBody:(CCBodySprite *)sprite2;
+-(void) setBody:(CCBodySprite *)sprite1 andBody:(CCBodySprite *)sprite2 atAnchor:(CGPoint)anchor;
+
+@end
